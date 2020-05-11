3d5c11f95e41e4ea5be03c3eaed8893d9079cf5d
MS_Akhtar <61315212+shoaibisa@users.noreply.github.com>
2020-05-11 22:42:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+
+  var numOfbutton = document.querySelectorAll(".drum").length;
+
+ for(var i=0; i<numOfbutton;i++)
+ document.querySelectorAll("button")[i].addEventListener("click", drumClick);
+
+ document.addEventListener("keypress", function(event) {
+   var keyDrum = event.key;
+
+     makeSound(keyDrum);
+
+ });
+
+  //button click
+  function drumClick()
+  {
+      var drumButton = this.innerHTML;
+
+      makeSound(drumButton);
+
+  }
+
+
+    function makeSound(key) {
+
+      switch (key) {
+        case 'w':
+        {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+          break;
+        }
+        case 'a':
+        {
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+          break;
+        }
+        case 's':
+        {
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+          break;
+        }
+        case 'd':
+        {
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+          break;
+        }
+        case 'j':
+        {
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+          break;
+        }
+        case 'k':
+        {
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+          break;
+        }
+        case 'l':
+        {
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+          break;
+        }
+
+        default:
+        {
+        console.log();
+        }
+
+      }
+
+    }
